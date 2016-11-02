3e3e9cc3ee75127c4184e351cad209e449212c2a
cheeseinlove <you@example.com>
2016-11-02 19:56:54 +0800

    1

---
+++ EasyNews-master/app/src/main/java/activity/TitleLayout.java
@@ -0,0 +1,39 @@
+package activity;
+
+import android.text.Layout;
+import android.widget.LinearLayout;
+import android.app.Activity;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+
+import android.widget.Toast;
+/**
+ * Created by 讯 on 2016/11/2.
+ */
+
+public class TitleLayout extends LinearLayout {
+
+    public TitleLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        LayoutInflater.from(context).inflate(R.layout.title, this);
+        Button btnTitleBack = (Button)findViewById(R.id.btn_back);
+       Button btnTitleEdit = (Button)findViewById(R.id.btn_edit);
+        btnTitleBack.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((Activity)getContext()).finish();  //销毁该Activity
+            }
+        });
+
+        btnTitleEdit.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "You clicked Edit Button", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
